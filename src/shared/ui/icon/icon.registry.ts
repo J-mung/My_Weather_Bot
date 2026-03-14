2cd18b5e381d771d428f5ea64c96d1d15d525b2d
J-mung <xorb2298@gmail.com>
2026-03-14 23:08:43 +0900

    :recycle: 날씨 아이콘 레지스트리를 정리

--- src/shared/ui/icon/icon.registry.ts
+++ src/shared/ui/icon/icon.registry.ts
@@ -2,12 +2,28 @@ const path = "/icons";
 
 export const iconRegistry = {
   addCircle: `${path}/icon_add_circle.svg`,
+  arrowDown: `${path}/icon_arrow_downward.svg`,
+  arrowUp: `${path}/icon_arrow_upward.svg`,
+  bookmark: `${path}/icon_bookmark.svg`,
   bookmarkAdd: `${path}/icon_bookmark_add.svg`,
   bookmard: `${path}/icon_bookmark.svg`,
+  cloudAlert: `${path}/icon_cloud_alert.svg`,
+  cloud: `${path}/icon_cloud.svg`,
   delete: `${path}/icon_delete.svg`,
   edit: `${path}/icon_edit.svg`,
   menu: `${path}/icon_menu.svg`,
   moreHoriz: `${path}/icon_more_horiz.svg`,
+  partlyCloudyDay: `${path}/icon_partly_cloudy_day.svg`,
+  rainy: `${path}/icon_rainy.svg`,
+  rainyHeavy: `${path}/icon_rainy_heavy.svg`,
+  rainyLight: `${path}/icon_rainy_light.svg`,
+  rainySnow: `${path}/icon_rainy_snow.svg`,
+  snowing: `${path}/icon_snowing.svg`,
+  snowingHeavy: `${path}/icon_snowing_heavy.svg`,
+  waterDrop: `${path}/icon_water_drop.svg`,
+  weatherMix: `${path}/icon_weather_mix.svg`,
+  weatherSnowy: `${path}/icon_weather_snowy.svg`,
+  wbSunny: `${path}/icon_wb_sunny.svg`,
 } as const;
 
 export type IconName = keyof typeof iconRegistry;
